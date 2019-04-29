47705cbb4fa86eff41d7149fd29f95e6d8c265f2
Didier <deramirez19@misena.edu.co>
2019-04-29 10:41:50 -0500

    xml

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,6 @@
 export const environment = {
   production: true,
-<<<<<<< HEAD
-  url: 'http://localhost:8000'
-  // url: 'https://upper.website'
+  // url: 'http://localhost:8000'
+  url: 'https://upper.website'
   // url: 'https://upper.dev'
-=======
-  // url: 'https://upper.website'
-  url: 'https://upper.dev'
->>>>>>> 3e2ac725083baed07e73bb2b49021e8a2dea931e
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,9 @@
 
 export const environment = {
   production: false,
-<<<<<<< HEAD
-  //  url: 'https://upper.website'
+   url: 'https://upper.website'
   // url: 'https://upper.dev'
-  url: 'http://localhost:8000'
-=======
-   // url: 'https://upper.website'
-    url: 'https://upper.dev'
->>>>>>> 3e2ac725083baed07e73bb2b49021e8a2dea931e
+  // url: 'http://localhost:8000'
 };
 
 /*
