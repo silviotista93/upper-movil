6b4cd3b91f601d829771c9720ce4ddbe9953c69e
Didier <deramirez19@misena.edu.co>
2019-05-09 08:02:29 -0500

    interface car

--- src/app/interfaces/interfaces.ts
+++ src/app/interfaces/interfaces.ts
@@ -11,6 +11,17 @@ export interface Usuario {
     social_acounts?: string;
 }
 
+export interface Car {
+    id?: string;
+    board?: string;
+    picture?: string;
+    car_type_id?: string;
+    cilindraje_id?: string;
+    brand_id?: string;
+    user_id?: string;
+    color_id?: string;
+}
+
 
 export interface ComponentMenu {
     title: string;
