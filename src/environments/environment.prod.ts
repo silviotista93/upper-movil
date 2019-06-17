1b9200c398aace1c7876a62bcc5e54d108dcf263
didier01 <deramirez19@misena.edu.co>
2019-06-17 14:16:25 -0500

    prueba camera ios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  url: 'http://localhost:8000'
+  // url: 'http://localhost:8000'
   //  url: 'http://192.168.43.199:8000'
-   //url: 'https://upper.website'
+   url: 'https://upper.website'
   //  url: 'https://upper.dev'
 };
